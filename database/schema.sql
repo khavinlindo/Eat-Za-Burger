3a046eeaf18bb8c305f53a7896061605874f1456
LindoK <khavin.lindo@gmail.com>
2019-10-02 02:08:54 -0400

    Updated DB schema

--- database/schema.sql
+++ database/schema.sql
@@ -5,6 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
